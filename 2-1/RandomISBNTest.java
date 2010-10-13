49d9b2319db57c7b93bbaba22d1753ca282cc050
wuschu <dominik.feller@students.unibe.ch>
2010-10-13 13:02:43 +0200

    changes in 2-1 / correct

---
+++ 2-1/RandomISBNTest.java
@@ -0,0 +1,19 @@
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class RandomISBNTest
+{
+	public static void main(String[] args)
+	{
+		RandomISBN isbn1, isbn2, isbn3;
+		
+		isbn1 = new RandomISBN();
+		System.out.println("1st ISBN is: " + isbn1);
+		isbn2 = new RandomISBN();
+		System.out.println("2nd ISBN is: " + isbn2);
+		isbn3 = new RandomISBN();
+		System.out.println("3rd ISBN is: " + isbn3);
+
+
+	}
+}
