a7c02463a9e122d0255048cca6c0946c2e04777c
Ubuntu <ubuntu@ubuntu-desktop.(none)>
2010-10-17 01:57:24 +0200

    Aufgaben Serie 2

--- 2-1/RandomISBNTest.java
+++ 2-1/RandomISBNTest.java
@@ -1,19 +1,29 @@
+//***************************************************************************
+//
+//	RandomISBNTest.java
+//	Dominik Feller
+//	Matrikel-Nr.: 06-117-949
+//
+//***************************************************************************
+
 import java.text.DecimalFormat;
 import java.util.Random;
 
 public class RandomISBNTest
 {
-    public static void main(String[] args)
-    {
-        int num1;
-        
-        Random gen = new Random();
-        num1 = gen.nextInt(10);
-        
-        DecimalFormat fmt = DecimalFormat(00);
-        System.out.println(num1);
-        
+	public static void main(String[] args)
+	{
+		// Generieren von 3 ISBN-Nummern mit Hilfe von RandomISBN
+		
+		RandomISBN isbn1, isbn2, isbn3;
+		
+		isbn1 = new RandomISBN();
+		System.out.println("1st ISBN is: " + isbn1);
+		isbn2 = new RandomISBN();
+		System.out.println("2nd ISBN is: " + isbn2);
+		isbn3 = new RandomISBN();
+		System.out.println("3rd ISBN is: " + isbn3);
 
-    }
 
+	}
 }
---
+++ 2-2/Book.java
@@ -0,0 +1,113 @@
+//****************************************************************************
+//
+//	Dominik Feller
+//	Matrikel-Nr. 06-117-949
+//
+//	Book.java
+//
+//****************************************************************************
+
+import java.util.Date;
+import java.text.*;
+import java.util.Scanner;
+
+
+public class Book{
+	private int id;
+	private String title;
+	private String author;
+	private Date dateOfPublication;
+
+	public static final String DATE_FORMAT = "dd.MM.yyyy";
+
+	// constructors
+	//TODO
+	public Book() throws ParseException
+	{
+		input();
+	}
+
+
+	/** Returns the age of the book in days since publication */
+	public int age(){
+		//TODO
+		long todaysDateMS, dateOfPublicationMS, diffMS, diffDays;
+		Date todaysDate = new Date();
+		todaysDateMS = todaysDate.getTime();
+		dateOfPublicationMS = dateOfPublication.getTime();
+		diffMS = todaysDateMS - dateOfPublicationMS;
+		diffDays = diffMS/1000/60/60/24;
+		return (int)diffDays; //this is to avoid compiler errors, replace it!
+	}
+
+	/** Returns a String representation of the book */
+	public String toString(){
+		String result = id+", "+title+", "+author+", "+
+			dateToString(dateOfPublication);
+		return result;
+	}
+
+	/** Reads all book data from user input */
+	public void input() throws ParseException{
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Please enter id: ");
+		id = scn.nextInt();
+		System.out.print("Please enter the title: ");
+		title = scn.next();
+		System.out.print("Please enter the name of the author: ");
+		author = scn.next();
+		System.out.print("Please enter the date of publication in "
+			+ "the format " + DATE_FORMAT + ": ");
+		String s = scn.next();
+		dateOfPublication = stringToDate(s);
+	}
+
+	// Get-/Set-methods
+	//TODO
+	public void setId (int bookid)
+	{
+		id = bookid;
+	}
+	public int getId()
+	{
+		return id;
+	}
+	public void setTitle (String booktitle)
+	{
+		title = booktitle;
+	}
+	public String getTitle()
+	{
+		return title;
+	}
+	public void setAuthor (String bookauthor)
+	{
+		author = bookauthor;
+	}
+	public String getAuthor()
+	{
+		return author;
+	}
+	public void setDateOfPublication (String bookdate) throws ParseException
+	{
+		dateOfPublication = stringToDate(bookdate);
+	}
+	public Date getDateOfPublication()
+	{
+		return dateOfPublication;
+	}
+	
+	
+	// private methods --------------------------------------------
+	/** Converts the Date object d into a String object */
+	private String dateToString(Date d){
+		SimpleDateFormat fmt = new SimpleDateFormat(DATE_FORMAT);
+		return fmt.format(d);
+	}
+
+	/** Converts the String object s into a Date object */
+	private Date stringToDate(String s) throws ParseException{
+		SimpleDateFormat fmt = new SimpleDateFormat(DATE_FORMAT);
+		return fmt.parse(s);
+	}
+}
