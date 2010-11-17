62c865b6fb14eaea9b06c621d7d68e52603a6806
wuschu <dominik.feller@students.unibe.ch>
2010-11-17 13:43:12 +0100

    done

---
+++ 4-2/IMedium.java
@@ -0,0 +1,13 @@
+
+public interface IMedium{
+	
+	/** returns the ID of the book */
+	public int getId();
+	
+	/** returns the price of the book */
+	public int getPrice();
+	
+	/** returns a short description */
+	public String getShortDescription();
+	
+}
